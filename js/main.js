7407eab49de0a3ff71773e2c91692ca9bea20a4e
“Katerina” <katerinakoptelova@yandex.ru>
2018-12-25 23:24:42 +0300

    Удаление листенеров
    1) Добавила removeEventListener. 2) Отсортировала переменные, функции, события.

--- js/main.js
+++ js/main.js
@@ -6,6 +6,34 @@ var mapElement = document.querySelector('.map');
 var pinsElement = mapElement.querySelector('.map__pins');
 var form = document.querySelector('.ad-form');
 
+var TITLES = [
+  'Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец',
+  'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик',
+  'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'
+];
+
+// форма объявления
+var formFields = document.querySelectorAll('.ad-form__element');
+
+// блокируем форму с фильтрами
+var formFilter = document.querySelector('.map__filters');
+formFilter.setAttribute('disabled', 'disabled');
+
+// активное состояние страницы
+var pinMain = document.querySelector('.map__pin--main');
+var adressPinMain = parseInt(pinMain.style.left, 10) + ', ' + parseInt(pinMain.style.top, 10);
+
+// инпут заголовок объявления
+var headerAdInput = document.getElementById('title');
+
+// инпут количество комнат
+var roomNumber = document.getElementById('room_number');
+
+// кнопка очистить
+var btnReset = document.querySelector('.ad-form__reset');
+
+
+// FUNCTION
 function randomInt(min, max) {
   return Math.floor(Math.random() * (max + 1 - min) + min);
 }
@@ -24,12 +52,6 @@ function createAuthor(number) {
     avatar: 'img/avatars/user' + formatNumber(number + 1, 2) + '.png'
   };
 }
-
-var TITLES = [
-  'Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец',
-  'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик',
-  'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'
-];
 
 function genUnique(array, length) {
   var result = [];
@@ -96,16 +118,6 @@ function createLocation(x) {
   };
 }
 
-// блокируем формы объявления
-var formFields = document.querySelectorAll('.ad-form__element');
-formFields.forEach(function (item) {
-  item.setAttribute('disabled', 'disabled');
-});
-
-// блокируем форму с фильтрами
-var formFilter = document.querySelector('.map__filters');
-formFilter.setAttribute('disabled', 'disabled');
-
 // функция создания одного объявления
 function createAd(number, title, x) {
   var ad = {
@@ -120,9 +132,7 @@ function createAd(number, title, x) {
       cardFragment.appendChild(renderCard(ad));
       mapElement.insertBefore(cardFragment, mapElement.querySelector('.map__filters-container'));
     }
-
   };
-
   ad.offer.address = ad.location.x.toString() + ', ' + ad.location.y;
   ad.offer.title = title;
   return ad;
@@ -138,7 +148,6 @@ function genAds() {
     titles.splice(j, 1);
     var mapWidth = document.querySelector('.map__pins').clientWidth;
     var ad = createAd(i, title, randomInt(0, mapWidth));
-
     ads.push(ad);
   }
 
@@ -214,23 +223,180 @@ function renderCard(ad) {
   return element;
 }
 
-// активное состояние страницы
-var pinMain = document.querySelector('.map__pin--main');
-var adressPinMain = parseInt(pinMain.style.left, 10) + ', ' + parseInt(pinMain.style.top, 10);
+// блокируем формы объявления
+formFields.forEach(function (item) {
+  item.setAttribute('disabled', 'disabled');
+});
 
 // функция добавляет адрес map__pin в инпут адреc
-function fillInputAdress() {
+function fillInputAddress() {
   var input = document.getElementById('address');
   input.setAttribute('value', adressPinMain);
 }
 
+// валидация формы тип жилья / цена
+function onSelectChanged(evt) {
+  switch (evt.target.value) {
+    case 'bungalo':
+      priceInput.placeholder = 0;
+      break;
+    case 'flat':
+      priceInput.placeholder = 1000;
+      break;
+    case 'house':
+      priceInput.placeholder = 5000;
+      break;
+    case 'palace':
+      priceInput.placeholder = 10000;
+      break;
+    default:
+      priceInput.placeholder = 0;
+      break;
+  }
+}
+
+// валидация форм время заезда и выезда
+function onLoad() {
+  document.getElementById('timein').onchange = onTimeChanged;
+  document.getElementById('timeout').onchange = onTimeChanged;
+}
+
+function onTimeChanged(evt) {
+  var timeoutInput = document.getElementById('timeout');
+  var timeinInput = document.getElementById('timein');
+
+  switch (evt.target.value) {
+    case '12:00':
+      timeoutInput.value = '12:00';
+      timeinInput.value = '12:00';
+      break;
+    case '13:00':
+      timeoutInput.value = '13:00';
+      timeinInput.value = '13:00';
+      break;
+    case '14:00':
+      timeoutInput.value = '14:00';
+      timeinInput.value = '14:00';
+      break;
+  }
+}
+
+// валидация форм количество комнат и мест
+function disabledCapacity(dis) {
+  var options = document.getElementById('capacity').getElementsByTagName('option');
+
+  for (var i = 0; i < options.length && i < dis.length; i++) {
+    options[i].disabled = dis[i];
+  }
+}
+
+function modifyCapacity() {
+  var capacity = document.getElementById('capacity');
+
+  if (roomNumber.value === '1') {
+    disabledCapacity([false, true, true, true]);
+    capacity.value = '1';
+  } else if (roomNumber.value === '2') {
+    disabledCapacity([false, false, true, true]);
+    capacity.value = '2';
+  } else if (roomNumber.value === '3') {
+    disabledCapacity([false, false, false, true]);
+    capacity.value = '3';
+  } else if (roomNumber.value === '100') {
+    disabledCapacity([true, true, true, false]);
+    capacity.value = '0';
+  }
+}
+
+function showUploadMessage() {
+  var template = document.querySelector('#success').content.querySelector('.success');
+  var element = template.cloneNode(true);
+  var btn = document.querySelector('.ad-form__submit');
+  btn.appendChild(element);
+  function onMessageEscPress(evt) {
+    if (evt.keyCode === 27) {
+      element.remove();
+      document.removeEventListener('keydown', onMessageEscPress);
+    }
+  }
+  document.addEventListener('keydown', onMessageEscPress);
+
+  function onElementRemove() {
+    element.remove();
+    messageOk.removeEventListener('click', onElementRemove);
+  }
+  var messageOk = document.querySelector('.success');
+  messageOk.addEventListener('click', onElementRemove);
+}
+
+
+function showErrorMessage() {
+  var template = document.querySelector('#error').content.querySelector('.error');
+  var element = template.cloneNode(true);
+  var btn = document.querySelector('.ad-form__submit');
+  btn.appendChild(element);
+  function onMessageEscPress(evt) {
+    if (evt.keyCode === 27) {
+      element.remove();
+      document.removeEventListener('keydown', onMessageEscPress);
+    }
+  }
+  document.addEventListener('keydown', onMessageEscPress);
+
+  var btnEsc = document.querySelector('.error__button');
+  function onErrorButtonClick() {
+    element.remove();
+    btnEsc.removeEventListener('click', onErrorButtonClick);
+  }
+  btnEsc.addEventListener('click', onErrorButtonClick);
+
+  var messageError = document.querySelector('.error');
+  function onElementRemove() {
+    element.remove();
+    messageError.removeEventListener('click', onElementRemove);
+  }
+  messageError.addEventListener('click', onElementRemove);
+}
+
+// сброс страницы при нажатии кнопки очистить
+function onButtonClick() {
+  var cards = document.querySelector('.map__card');
+  cards.remove();
+  var pinOther = document.querySelectorAll('.pinOther');
+  pinOther.forEach(function (item) {
+    item.remove();
+  });
+  fillInputAddress();
+  btnReset.removeEventListener('click', onButtonClick);
+}
+
+
+// EventListener
+// отправляем данные на сервер
+form.addEventListener('submit', function (ev) {
+  var data = new FormData(form);
+  var http = new XMLHttpRequest();
+  http.open(form.method, form.action, true);
+  http.onload = function () {
+    if (http.status === 200) {
+      showUploadMessage();
+      form.reset();
+    } else {
+      showErrorMessage();
+    }
+  };
+  http.send(data);
+  ev.preventDefault();
+}, false);
+
+// нажатие на главную метку - страница в активном состоянии
 pinMain.addEventListener('mouseup', function () {
   document.querySelector('.map').classList.remove('map--faded');
   document.querySelector('.ad-form').classList.remove('ad-form--disabled');
   formFields.forEach(function (item) {
     item.removeAttribute('disabled', 'disabled');
   });
-  fillInputAdress();
+  fillInputAddress();
   var pinsFragment = document.createDocumentFragment();
   for (var i = 0; i < ads.length; i++) {
     pinsFragment.appendChild(renderPin(ads[i]));
@@ -239,7 +405,6 @@ pinMain.addEventListener('mouseup', function () {
 });
 
 // валидация формы заголовок объявления, сообщения об ошибке
-var headerAdInput = document.getElementById('title');
 headerAdInput.addEventListener('invalid', function () {
   if (headerAdInput.validity.tooShort) {
     headerAdInput.setCustomValidity('Минимальная длина объявления — 30 символов');
@@ -287,148 +452,12 @@ document.addEventListener('DOMContentLoaded', function () {
   document.getElementById('type').onchange = onSelectChanged;
 }, false);
 
-function onSelectChanged(evt) {
-  switch (evt.target.value) {
-    case 'bungalo':
-      priceInput.placeholder = 0;
-      break;
-    case 'flat':
-      priceInput.placeholder = 1000;
-      break;
-    case 'house':
-      priceInput.placeholder = 5000;
-      break;
-    case 'palace':
-      priceInput.placeholder = 10000;
-      break;
-    default:
-      priceInput.placeholder = 0;
-      break;
-  }
-}
+// клик на кнопку очистить
+btnReset.addEventListener('click', onButtonClick);
 
-// валидация форм время заезда и выезда
-function onLoad() {
-  document.getElementById('timein').onchange = onTimeChanged;
-  document.getElementById('timeout').onchange = onTimeChanged;
-}
-
+// изменения инпутов время заезда и выезда
 document.addEventListener('DOMContentLoaded', onLoad, false);
 
-function onTimeChanged(evt) {
-  var timeoutInput = document.getElementById('timeout');
-  var timeinInput = document.getElementById('timein');
-
-  switch (evt.target.value) {
-    case '12:00':
-      timeoutInput.value = '12:00';
-      timeinInput.value = '12:00';
-      break;
-    case '13:00':
-      timeoutInput.value = '13:00';
-      timeinInput.value = '13:00';
-      break;
-    case '14:00':
-      timeoutInput.value = '14:00';
-      timeinInput.value = '14:00';
-      break;
-  }
-}
-
-//   валидация форм количество комнат и мест
-var roomNumber = document.getElementById('room_number');
-
-function disabledCapacity(dis) {
-  var options = document.getElementById('capacity').getElementsByTagName('option');
-
-  for (var i = 0; i < options.length && i < dis.length; i++) {
-    options[i].disabled = dis[i];
-  }
-}
-
-function modifyCapacity() {
-  var capacity = document.getElementById('capacity');
-
-  if (roomNumber.value === '1') {
-    disabledCapacity([false, true, true, true]);
-    capacity.value = '1';
-  } else if (roomNumber.value === '2') {
-    disabledCapacity([false, false, true, true]);
-    capacity.value = '2';
-  } else if (roomNumber.value === '3') {
-    disabledCapacity([false, false, false, true]);
-    capacity.value = '3';
-  } else if (roomNumber.value === '100') {
-    disabledCapacity([true, true, true, false]);
-    capacity.value = '0';
-  }
-}
+// клик на инпуты количество комнат и мест
 roomNumber.addEventListener('change', modifyCapacity);
 document.addEventListener('DOMContentLoaded', modifyCapacity, false);
-
-
-// отправляем данные на сервер
-form.addEventListener('submit', function (ev) {
-  var data = new FormData(form);
-  var http = new XMLHttpRequest();
-  http.open(form.method, form.action, true);
-  http.onload = function () {
-    if (http.status === 200) {
-      showUploadMessage();
-      form.reset();
-    } else {
-      showErrorMessage();
-    }
-  };
-  http.send(data);
-  ev.preventDefault();
-}, false);
-
-function showUploadMessage() {
-  var template = document.querySelector('#success').content.querySelector('.success');
-  var element = template.cloneNode(true);
-  var btn = document.querySelector('.ad-form__submit');
-  btn.appendChild(element);
-  document.addEventListener('keydown', function (evt) {
-    if (evt.keyCode === 27) {
-      element.remove();
-    }
-  });
-  var messageOk = document.querySelector('.success');
-  messageOk.addEventListener('click', function () {
-    element.remove();
-  });
-}
-
-function showErrorMessage() {
-  var template = document.querySelector('#error').content.querySelector('.error');
-  var element = template.cloneNode(true);
-
-  var btn = document.querySelector('.ad-form__submit');
-  btn.appendChild(element);
-  document.addEventListener('keydown', function (evt) {
-    if (evt.keyCode === 27) {
-      element.remove();
-    }
-  });
-  var btnEsc = document.querySelector('.error__button');
-  btnEsc.addEventListener('click', function () {
-    element.remove();
-  });
-  var messageError = document.querySelector('.error');
-  messageError.addEventListener('click', function () {
-    element.remove();
-  });
-}
-
-// сброс страницы при нажатии кнопки очистить
-var btnReset = document.querySelector('.ad-form__reset');
-btnReset.addEventListener('click', function () {
-  var cards = document.querySelector('.map__card');
-  cards.remove();
-  var pinOther = document.querySelectorAll('.pinOther');
-  pinOther.forEach(function (item) {
-    item.remove();
-  });
-  fillInputAdress();
-});
